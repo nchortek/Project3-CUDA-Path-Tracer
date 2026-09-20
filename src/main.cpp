747deea004388b1eb4dbe6b9795c7a7376c5ac62
Nathan Chortek <nathanchortek@gmail.com>
2026-09-20 14:05:51 -0400

    Delete unneeded base code

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include "ImGui/imgui.h"
-#include "ImGui/imgui_impl_glfw.h"
-#include "ImGui/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_vulkan.h"
 
 #include <cuda_runtime.h>
 #include <cuda_gl_interop.h>
--- src/pathtrace.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "scene.h"
-#include "utilities.h"
-
-void InitDataContainer(GuiDataContainer* guiData);
-void pathtraceInit(Scene *scene);
-void pathtraceFree();
-void pathtrace(uchar4 *pbo, int frame, int iteration);
